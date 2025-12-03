2dde10ef4e063e6ae27f714cc584bb048e4155f6
asliiiisaa5-cyber <asliiiisaa5@gmail.com>
2025-12-03 22:17:52 +0300

    Add files via upload

---
+++ 4 problem.py
@@ -0,0 +1,31 @@
+# problem 1
+x = int(input())
+if x%2 == 0:
+    print("even")
+else:
+    print("odd")    
+# problem 2
+x = int(input())
+y = int(input())
+if x > y:
+    print(x-y)
+else:
+    print(x+y)
+# problem 3
+x = int(input())
+y = int(input())
+z = int(input())
+if x > 0:
+    k = x*y
+else:
+    k = x-y
+if z%2 == 0:
+    w = k+z
+else:
+    w = z-k
+print(abs(w))
+
+# problem 4
+x = int(input()) 
+print(abs(×)
+    
